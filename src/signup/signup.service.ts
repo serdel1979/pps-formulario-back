fcf8bef94984d8e65d586185d8259cebd46badad
Sergio De Luca <sdlbsso@gmail.com>
2022-10-03 13:53:32 -0300

    Validación de un formulario en el servidor

--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSignupDto } from './dto/create-signup.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class SignupService {
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+
   async create(createSignupDto: CreateSignupDto):Promise<CreateSignupDto>{
-    console.log("Guardado en base de datos ",createSignupDto);
-    return createSignupDto;
+    if (createSignupDto.password !== createSignupDto.repeatPass){
+      throw new NotFoundException('Invalid data'); 
+    }
+    const hash = await this.hashPassword(createSignupDto.password);
+    let user = {...createSignupDto, password: hash}
+    console.log("Guardado en base de datos ",user);
+    return user;
   }
 
 
